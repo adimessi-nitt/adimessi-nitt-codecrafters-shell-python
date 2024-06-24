4280cdce33e54adf60186b73b0432fe483d5e65b
adimessi-nitt <adithyapradeep40882@gmail.com>
2024-06-24 06:07:52 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -11,6 +11,9 @@ def main():
         command = input()
         if(command =="exit 0"):
             return 
+        elif (command.find("echo")==0):
+            content_start_index = command.find("o")
+            sys.stdout.write(f"{command[content_start_index+2:]}\n")
         else:
             sys.stdout.write(f"{command}: command not found\n")
 

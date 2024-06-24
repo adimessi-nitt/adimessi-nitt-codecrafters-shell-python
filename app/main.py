3501d17e166e44d05bdd20916448c62770731a15
adimessi-nitt <adithyapradeep40882@gmail.com>
2024-06-24 19:09:22 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
+from email.errors import NonPrintableDefect
+from pathlib import Path
 import sys
-from urllib import request
-
-
-
+import os
 
 def main():
+    PATH  = os.environ.get("PATH")
 
     while(True):
         sys.stdout.write("$ ")
         sys.stdout.flush()
         command = input()
-        type = ["cat", "exit", "echo", "type"]
+        built_in = ["exit", "echo", "type"]
         if(command =="exit 0"):
             return 
         elif (command.find("echo")==0):
@@ -18,12 +18,20 @@ def main():
             sys.stdout.write(f"{command[content_start_index+2:]}\n")
         elif(command.find("type")==0):
             word, content = command.split(" ")
-            if content in type:
+            Paths = PATH.split(":")
+            cmd_path  = None
+            for path in Paths:
+                if os.path.isfile(f"{path}/{content}"):
+                    cmd_path = f"{path}/{content}"
+            if content in built_in:
                 sys.stdout.write(f"{content} is a shell builtin\n")
+            elif cmd_path:
+                sys.stdout.write(f"{content} is {cmd_path}\n")
             else:
                 sys.stdout.write(f"{content}: not found\n")
         else:
             sys.stdout.write(f"{command}: not found\n")
+        sys.stdout.flush()
 
 if __name__ == "__main__":
     main()

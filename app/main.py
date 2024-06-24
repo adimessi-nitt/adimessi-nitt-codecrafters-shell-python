15512867ce8525be6d3caa8ceb02fb8eea3d28d0
adimessi-nitt <adithyapradeep40882@gmail.com>
2024-06-24 06:23:30 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import sys
+from urllib import request
 
 
 
@@ -9,13 +10,20 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
         command = input()
+        type = ["cat", "exit", "echo", "type"]
         if(command =="exit 0"):
             return 
         elif (command.find("echo")==0):
             content_start_index = command.find("o")
             sys.stdout.write(f"{command[content_start_index+2:]}\n")
+        elif(command.find("type")==0):
+            word, content = command.split(" ")
+            if content in type:
+                sys.stdout.write(f"{content} is a shell builtin\n")
+            else:
+                sys.stdout.write(f"{content}: not found\n")
         else:
-            sys.stdout.write(f"{command}: command not found\n")
+            sys.stdout.write(f"{command}: not found\n")
 
 if __name__ == "__main__":
     main()
